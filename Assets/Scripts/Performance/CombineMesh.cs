efedd5dbe3c1b52daf50198d01646049c4ab77eb
LuukBukkems <Luuk.bukkems@hotmail.com>
2016-06-10 17:26:27 +0200

    try Cinnut

---
+++ Assets/Scripts/Performance/CombineMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombineMesh : MonoBehaviour {
+
+        MeshFilter[] meshFilters = transform.getChildren().GetComponentsInChildren<MeshFilter>;
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length) {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.active = false;
+            i++;
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.active = true;
+}
